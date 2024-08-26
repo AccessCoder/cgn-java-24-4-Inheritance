546e8a97e5036edb2032a71cfdcef67242ed190f
Dominic <andro5@web.de>
2024-08-26 10:44:28 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/Main.java
@@ -0,0 +1,30 @@
+package de.neuefische;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Guitar guitar = new Guitar("Fender", "Fender", 159.33, "Black", true);
+        guitar.setManufacturer("Gibson");
+
+        Piano piano = new Piano("", "", 3400, "Black", 5);
+        Trumpet trumpet = new Trumpet("", "", 150, "Gold", true);
+
+
+
+        playInstrument(guitar); //Polymorphism
+        playInstrument(piano);
+        playInstrument(trumpet);
+
+        Instrument[] musicStore = {guitar, trumpet, piano};
+
+
+    }
+
+    public static void playInstrument(Instrument instrument){
+        instrument.makeNoise();
+
+    }
+
+
+}
---
+++ src/main/java/de/neuefische/Trumpet.java
@@ -0,0 +1,16 @@
+package de.neuefische;
+
+public class Trumpet extends Instrument{
+
+    private boolean hasCase;
+
+    public Trumpet(String manufacturer, String brand, double price, String color, boolean hasCase) {
+        super(manufacturer, brand, price, color);
+        this.hasCase = hasCase;
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Trumpet Sounds");
+    }
+}
